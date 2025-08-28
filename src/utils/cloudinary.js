c7f36e82b7bd105f54e0cd6287e2c46ab61a7893
Kailash Kumawat <kumawatkai369@gmail.com>
2025-08-28 18:10:58 +0530

    add file handling setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCoudinary = async function (localFilePath) {
+  try {
+    if (!localFilePath) return null;
+
+    //UPLOAD THE FILE ON CLOUDINARY
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    //FILE SUCCESFULLY UPLOADED
+    console.log("File is uploaded on cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // REMOVE THE LOCALLY SAVED TEMP. FILE AS THE
+    // UPLOAD OPERATION GOT FAILED.
+    return null;
+  }
+};
+
+export { uploadOnCoudinary };
