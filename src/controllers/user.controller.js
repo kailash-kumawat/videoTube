00538191102bcae52758bdb4e4a4c0962100efe1
Kailash Kumawat <kumawatkai369@gmail.com>
2025-09-06 18:43:01 +0530

    Add getUserChannelProfile endpoint to retrieve user channel details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -326,6 +326,84 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Cover image updated successfully"));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  if (!username?.trim()) {
+    throw new ApiError(400, "Username is missing");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+
+    {
+      $lookup: {
+        from: "subscriptions", // used naming convention from mongoDB.
+        foreignField: "channel",
+        localField: "_id",
+        as: "subscribers",
+      },
+    },
+
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+
+    {
+      $addFields: {
+        subscribersCount: {
+          // count subscribers.
+          $size: "$subscribers",
+        },
+
+        channelsSubscribedTo: {
+          $size: "$subscribedTo",
+        },
+
+        isSubscribed: {
+          // user subscribe channel or not.
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+
+      // show only selected fields.
+      $project: {
+        fullName: 1,
+        username: 1,
+        email: 1,
+        avatar: 1,
+        coverImage: 1,
+        subscribersCount: 1,
+        channelsSubscribedTo: 1,
+        isSubscribed: 1,
+      },
+    },
+  ]);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "channel does not exist");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "User channek fetched successfully"),
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -336,4 +414,5 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
 };
